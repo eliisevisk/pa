327021ceac9f37fc90f7b8dc98242a2e47ab2706
Eliise Visk <eliise.visk@khk.ee>
2019-09-25 10:43:57 +0300

    Ülesanne 1, üks võimalik lahendus.

---
+++ 25.09.2019/ex1.php
@@ -0,0 +1,9 @@
+<?php
+/* Eesnimi, Perenimi, e-mail, kui vana olete, pikkus M, kaal kg; */
+print "<h1> Info minust</h1>";
+print "Eesnimi: Eliise<br>";
+print "Perenimi: Visk <br>";
+print "e-mail: eliise.visk@khk.ee <br>";
+print "Vanus: 23<br>";
+print "Pikkus: 1.71m<br>";
+print "Kaal: 67kg<br>";
