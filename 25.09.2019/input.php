441606dd4555a3584d16563e163a2cbd39f43321
Eliise Visk <eliise.visk@khk.ee>
2019-09-25 11:28:22 +0300

    Input ja input_do. Kasutajasisestus ja HTML vormid. GET meetod.

---
+++ 25.09.2019/input.php
@@ -0,0 +1,7 @@
+<form action="input_du.php" method="get">
+    <div>
+        <label for="nimi">Nimi:</label>
+        <input type="text" id="nimi" name="nimi">
+    </div>
+    <input type="submit" value="Saada">
+</form>
---
+++ 25.09.2019/input_du.php
@@ -0,0 +1,6 @@
+<?php
+//muutuja ja v''rtuse v]tmine vormist.
+$nimi = $_GET['nimi']; //<input /...../ name="nimi">
+print 'Nimi: '.$nimi.'<br>';
+print '<hr>';
+print '<a href="input.php">Sisesta andmed uuesti!</a>';
