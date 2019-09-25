690ec19db65d7bf9bcce7b25f1f7766a29c720a7
Eliise Visk <eliise.visk@khk.ee>
2019-09-25 11:36:34 +0300

    Input ja input_do. Kasutajasisestus ja HTML vormid. GET meetod.

--- 25.09.2019/input.php
+++ 25.09.2019/input.php
@@ -3,5 +3,25 @@
         <label for="nimi">Nimi:</label>
         <input type="text" id="nimi" name="nimi">
     </div>
+    <div>
+        <label for="perenimi">Perenimi:</label>
+        <input type="text" id="perenimi" name="perenimi">
+    </div>
+    <div>
+        <label for="email">E-mail:</label>
+        <input type="text" id="email" name="email">
+    </div>
+    <div>
+        <label for="kaal">Kaal:</label>
+        <input type="kaal" id="kaal" name="kaal">
+    </div>
+    <div>
+        <label for="pikkus">Pikkus:</label>
+        <input type="pikkus" id="pikkus" name="pikkus">
+    </div>
+    <div>
+        <label for="vanus">Vanus:</label>
+        <input type="vanus" id="vanus" name="vanus">
+    </div>
     <input type="submit" value="Saada">
 </form>
--- 25.09.2019/input_du.php
+++ 25.09.2019/input_du.php
@@ -1,6 +1,14 @@
 <?php
 //muutuja ja v''rtuse v]tmine vormist.
 $nimi = $_GET['nimi']; //<input /...../ name="nimi">
+$pikkus = $_GET['pikkus'];
+$kaal = $_GET['kaal'];
+$perenimi = $_GET['perenimi'];
+$vanus = $_GET['vanus'];
 print 'Nimi: '.$nimi.'<br>';
+print 'Perenimi: '.$perenimi.'<br>';
+print 'Vanus: '.$vanus.'<br>';
+print 'Kaal: '.$kaal.'<br>';
+print 'Pikkus: '.$pikkus.'<br>';
 print '<hr>';
 print '<a href="input.php">Sisesta andmed uuesti!</a>';
