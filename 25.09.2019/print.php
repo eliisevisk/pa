18d3138d9392108cb90076ef75c49fb7b6bde545
Eliise Visk <eliise.visk@khk.ee>
2019-09-25 10:31:06 +0300

    Tekstiväljastus php's koos HTML elementidega

--- 25.09.2019/print.php
+++ 25.09.2019/print.php
@@ -11,3 +11,8 @@
  * Dokumentatsiooni-
  * kommentaar
  * */
+
+//Kuidas toimub väljastus php's
+print "Tere maailm <br>";
+echo "Mingi tekst veel <br>";
+echo 'Veel mingi tekst, ja veel üks reavahetus <br>';
