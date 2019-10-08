b5ad50dbb8150d9b32f60d860330d76f87b44314
Eliise Visk <eliise.visk@khk.ee>
2019-10-08 09:05:40 +0300

    if-else lause

---
+++ 08.10.2019/pos-neg.php
@@ -0,0 +1,18 @@
+<?php
+
+// defineerime muutujad
+$number = -25;
+
+//control the value of statement
+//var_dump($number>0);
+//var_dump($number<0);
+// if statement to affirm $number positive value.
+if($number>0){
+    echo $number.' on positiivne!';
+}
+else{
+    echo $number.' on negatiivne';
+}
+echo '<br>';
+//the program is continued.
+echo 'Programmi lõpp';
